d21255dffd79880d6d9c3670855594ceab123bed
Battleman <olivier.cloux@epfl.ch>
2016-04-09 14:30:39 +0200

    makefile, stylish et création de astyle_orig

--- done/pictDBM/stylish.sh
+++ done/pictDBM/stylish.sh
@@ -1,11 +1,19 @@
 for i in *.c
 do
+	echo -e "-----------------------\n"
 	echo "Making $i sooo stylish"
 	astyle -A8 $i
 done
 
 for i in *.h
 do
+	echo -e "-----------------------\n"
 	echo "Making $i sooo stylish"
 	astyle -A8 $i
+done
+
+for i in *.orig
+do
+	echo -e "-----------------------\nDéplacement de $i dans le dossier des originaux (remplacera le fichier de même nom si existant)"
+	mv $i astyle_orig
 done
