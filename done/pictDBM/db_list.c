6d02107f35352c6c27f2a744237689038e08c5e1
Battleman <olivier.cloux@epfl.ch>
2016-04-11 14:22:48 +0200

    tentative de correction de erreurs week04

--- done/pictDBM/db_list.c
+++ done/pictDBM/db_list.c
@@ -5,8 +5,8 @@ void do_list(struct pictdb_file* myfile)
     int empty = 1; //check if there is an entry in the database
 
     print_header(&myfile->header); //anyway : print the header
-    for(int i = 0; i < MAX_MAX_FILES; ++i) { //go through the whole database
-        if(myfile -> metadata[i].is_valid != 0) {
+    for(size_t i = 0; i < MAX_MAX_FILES; ++i) { //go through the whole database
+        if(myfile -> metadata[i].is_valid == NON_EMPTY) {
             print_metadata(&(*myfile).metadata[i]);
             empty = 0;
         }
