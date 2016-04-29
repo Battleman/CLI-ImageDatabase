3a8a20edc23c6082a3818aa6e538f595409ea46a
Othman BENCHEKROUN <othman.benchekroun@epfl.ch>
2016-04-29 13:58:03 +0200

    Correction de do_delete

--- done/pictDBM/db_delete.c
+++ done/pictDBM/db_delete.c
@@ -5,14 +5,14 @@
 #include "error.h"
 #include "pictDB.h"
 
-static int modify_reference(const char* filename, FILE* fpdb, struct pict_metadata* meta_table);
+static int modify_reference(const char* filename, FILE* fpdb, struct pict_metadata* meta_table, size_t size);
 static int modify_header(FILE* fpdb, struct pictdb_header* header);
 
 int do_delete(const char* pic_name, struct pictdb_file* file)
 {
     int err = 0;
     if(file->header.num_files > 0) { //éviter des calculs si la base est vide
-		err = modify_reference(pic_name, file -> fpdb, file -> metadata);
+		err = modify_reference(pic_name, file -> fpdb, file -> metadata, file -> header.max_files);
 		if(err == 0) {
 			err = modify_header(file -> fpdb, &file -> header);
 		}
@@ -20,24 +20,23 @@ int do_delete(const char* pic_name, struct pictdb_file* file)
     return err;
 }
 
-static int modify_reference(const char* pic_name, FILE* fpdb, struct pict_metadata* meta_table)
+static int modify_reference(const char* pic_name, FILE* fpdb, struct pict_metadata* meta_table, size_t size)
 {
     size_t index = 0;
     int valid = 0;
     do {
-        if(strcmp(pic_name, meta_table[index].pict_id) == 0) { //comparaison entre le nom donné en argument et le nom de chaque metadata
+        if(meta_table[index].is_valid == 1 && strcmp(pic_name, meta_table[index].pict_id) == 0) { //comparaison entre le nom donné en argument et le nom de chaque metadata
             meta_table[index].is_valid = 0; //modification du bit de validité
             valid = 1; //indication qu'un match a été trouvé
         } else {
             index++;
         }
-    } while(valid == 0 && index < MAX_MAX_FILES); //itération jusq'à la taille de meta_table ou jusqu'à trouver un match
+    } while(valid == 0 && index < size); //itération jusq'à la taille de meta_table ou jusqu'à trouver un match
 
     if(valid == 0) {
-        return ERR_INVALID_PICID;
+        return ERR_FILE_NOT_FOUND;
     } else {
-        fseek(fpdb, sizeof(struct pictdb_header), SEEK_SET); //déplace la tête de lecture après le header
-        fseek(fpdb, index*sizeof(struct pict_metadata), SEEK_CUR); //déplace la tête au niveau du metadata à lire (ième)
+        fseek(fpdb, sizeof(struct pictdb_header) + index * sizeof(struct pict_metadata), SEEK_SET); //déplace la tête de lecture
         valid = fwrite(&meta_table[index], sizeof(struct pict_metadata), 1, fpdb); //ecriture au niveau de la tête, avec test de validité
     }
 
@@ -52,7 +51,10 @@ static int modify_header(FILE* fpdb, struct pictdb_header* header)
     header->num_files--; //réduction du nombre de fichiers
     header->db_version++; //version suivante
     rewind(fpdb); //retour au début du fichier pour écrire sur le header
-    fwrite(header, sizeof(struct pictdb_header), 1, fpdb);
+    
+    if(1 != fwrite(header, sizeof(struct pictdb_header), 1, fpdb)){
+		return ERR_IO;
+	}
 
     return 0;
 }
