f3f18a61926c72115ed98f031830c75b2aea28b5
Battleman <olivier.cloux@epfl.ch>
2016-11-28 10:54:29 +0100

    removed directories not to track

--- grades/week08/dedup.h
+++
@@ -1,7 +0,0 @@
-#include "pictDB.h"
-
-/* Correcteur : entête doxygen et doc doxygen de la fonction
- * manquants
- * -0.5 -0.5pt
- */
-int do_name_and_content_dedup(struct pictdb_file* db_file, uint32_t index);
